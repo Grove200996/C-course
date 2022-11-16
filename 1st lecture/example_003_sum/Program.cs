c40818970a7795391df70a47476e11d26c28303e
Eugene S <100590566+Grove200996@users.noreply.github.com>
2022-11-16 10:36:31 +0300

    Delete 1st lecture directory

--- 1st lecture/example_003_sum/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int FNumber = new Random().Next(1,10);
-Console.WriteLine(FNumber);
-int SNumber = new Random().Next(1,10);
-Console.WriteLine(SNumber);
-int Result = FNumber + SNumber;
-Console.WriteLine(Result);
