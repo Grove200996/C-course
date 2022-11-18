6a940bc7130e22cc9cec055a7601893019c0c589
Grove <gunner_777@mail.ru>
2022-11-18 22:54:00 +0300

    third webinar tasks + hw

---
+++ homework/C_Webinar_3/lesson3_task_19/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+void IsPalindrome(int number)
+{
+    int fifthDig = number % 10;
+    int fourthDig = (number / 10) % 10;
+    int thirdDig = (number / 100) % 10;
+    int secondDig = (number / 1000) % 10;
+    int firstDig = number / 10000;
+    if (fifthDig == firstDig && fourthDig == secondDig)
+    {
+        System.Console.WriteLine("Yes, it's palindrome");
+
+    }
+    else
+    {
+        System.Console.WriteLine("No,It's not");
+
+    }
+}
+IsPalindrome(23432);
---
+++ webinars/webinar_3/Program.cs
@@ -0,0 +1,89 @@
+﻿// Console.WriteLine("Введите координаты точки");
+// Console.Write("X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// int Quarter(int xc, int yc)
+// {
+//     if(xc > 0 && yc > 0) return 1;
+//     if(xc < 0 && yc > 0) return 2;
+//     if(xc < 0 && yc < 0) return 3;
+//     if(xc > 0 && yc < 0) return 4;
+//     return 0;
+// }
+
+// int quarter = Quarter(x, y);
+// string result = quarter > 0
+//         ? $"Указанные координаты соответствуют четверти -> {quarter}"
+//         : "Введены некорректны координаты";
+
+// Console.WriteLine(result);
+
+
+// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+// Console.Write("Введите номер четверти: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+
+// string Quarter(int num)
+// {
+//     if (num == 1) return "x > 0 and y > 0";
+//     if (num == 2) return "x < 0 and y > 0";
+//     if (num == 3) return "x < 0 and y < 0";
+//     if (num == 4) return "x > 0 and y < 0";
+//     return "Введены некорректны координаты";
+// }
+
+// string res = Quarter(number);
+// Console.WriteLine(res);
+
+
+// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+
+
+
+// System.Console.WriteLine("Введите первую точку: ");
+// double xa = Convert.ToDouble(Console.ReadLine());
+// double xb = Convert.ToDouble(Console.ReadLine());
+
+// System.Console.WriteLine("Введите вторую точку: ");
+// double ya = Convert.ToDouble(Console.ReadLine());
+// double yb = Convert.ToDouble(Console.ReadLine());
+
+// double Theorem(double x1, double y1, double x2, double y2)
+// {
+//     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+// }
+// double result = Theorem(xa, xb, ya, yb);
+
+// System.Console.WriteLine(Math.Round(result, 2, MidpointRounding.ToZero));
+
+
+
+
+// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+// void Mul(int number)
+// {
+//     for (int i = 1; i <= number; i++)
+//     {
+//         System.Console.WriteLine($"{i,4} = {i * i,4}");
+
+//     }
+
+// }
+
+// Mul(10);
