314914996be885cde70ac6d325ad1a9efbf61b59
Grove <gunner_777@mail.ru>
2022-11-23 16:32:36 +0300

    brought to perfection

--- homework/C_Webinar_4/lesson4_task_29/Program.cs
+++ homework/C_Webinar_4/lesson4_task_29/Program.cs
@@ -3,21 +3,46 @@
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 
-int[] array = new int[8];
-void FillArray(int[] arr)
+
+void FillArray(int[] arr, int a, int b)
 {
-    
+
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(-20, 20);
+        arr[i] = new Random().Next(a, b);
     }
 
 }
 void PrintArray(int[] arr)
 {
     Console.Write("[");
-    string res = string.Join(", ", array);
-    Console.Write($"{res}]");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+
+    }
+    Console.Write("]");
 }
-FillArray(array);
-PrintArray(array);
+
+
+System.Console.Write("Enter a number of digits: ");
+int amount = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int[] array = new int[amount];
+
+bool IsTrue = true;
+while (IsTrue)
+{   
+    System.Console.Write("Range start: ");
+    int start = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Range finish: ");
+    int finish = Convert.ToInt32(Console.ReadLine());
+    if (start < finish)
+    {
+        FillArray(array, start, finish);
+        PrintArray(array);
+        IsTrue = false;
+    }
+    else System.Console.WriteLine("wrong data");
+}
+
