278fac257bdb7971ce6bebfd8c4dc28b7fafb56c
Grove <gunner_777@mail.ru>
2022-12-11 21:09:50 +0300

    ninth

---
+++ homework/C_Webinar_9/lesson9_task_66/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int SumOfNaturalNumbers(int num1, int num2)
+{
+
+    if (num1 >= num2) return num1 + SumOfNaturalNumbers(num1 - 1, num2);
+    else return 0;
+}
+int SumOfNaturalNumbers1(int num1, int num2)
+{
+
+    if (num1 <= num2) return num1 + SumOfNaturalNumbers1(num1 + 1, num2);
+    else return 0;
+}
+
+
+
+System.Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (m > n)
+{
+    int result = SumOfNaturalNumbers(m, n);
+    System.Console.WriteLine(result);
+}
+else {
+    int result1 = SumOfNaturalNumbers1(m, n);
+    System.Console.WriteLine(result1);
+}
---
+++ webinars/webinar_9/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// System.Console.Write("Введите натуральное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+
+// void NaturalNumber(int num)
+// {
+//     if (num == 0) return;
+//     Console.Write($"{num} ");
+//     NaturalNumber(num - 1);
+// }
+
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+// System.Console.Write("Введите натуральное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите еще одно натуральное число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+
+// void NaturalNumber(int num1,int num2)
+// {
+//     if (num1 > num2)
+//     {
+//         Console.Write($"{num1} ");
+//         NaturalNumber(num1 - 1, num2);
+//     }
+//     else if (num1 < num2 )
+//     {   
+//         Console.Write($"{num1} ");
+//         NaturalNumber(num1 + 1,num2);
+//     }
+//     else Console.Write($"{num1} ");
+// }
+
+// NaturalNumber(n,m);
+
+
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+// System.Console.Write("Введите натуральное число: ");
+// int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+// int SumOfDigits(int num)
+// {
+//     if(num == 0) return 0;
+//     return num % 10 + SumOfDigits(num / 10);
+// }
+
+// System.Console.WriteLine(SumOfDigits(n));
+
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+
+System.Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите еще одно натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+int Exponentiate(int num1, int num2)
+{
+    if(num2 == 1) return num1;
+    return num1 * Exponentiate(num1,num2 - 1);
+}
+System.Console.WriteLine(Exponentiate(n,m));
